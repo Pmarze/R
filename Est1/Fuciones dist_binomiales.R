45b59574719322b3510c2a1aa7c63fc1a7dee1f6
Pmarze <53276103+Pmarze@users.noreply.github.com>
2021-02-25 22:29:01 -0600

    Update Fuciones dist_binomiales.R

--- Est1/Fuciones dist_binomiales.R
+++ Est1/Fuciones dist_binomiales.R
@@ -1,13 +1,17 @@
 ################# Funciónes binomiales #################
-lbinomial <-function(y,n,p,imprimir){
+lbinomial <-function(y,n,p,imprimir){       #P(x<y)
   leqbinomial(y-1,n,p,imprimir)
 }
 
-gbinomial <-function(y,n,p,imprimir){
+gbinomial <-function(y,n,p,imprimir){       #P(x>y)
   geqbinomial(y+1,n,p,imprimir)
 }
 
-leqbinomial <-function(y,n,p,imprimir){
+lgbinomial <-function(y1,y2,n,p,imprimir){  #P(y1<x<y2)
+  lgqbinomial(y1-1,y2+1,n,p,imprimir)
+}
+
+leqbinomial <-function(y,n,p,imprimir){     #P(x<=y)
   a=0
   for(i in 0:y){
     a<-a+dbinom(i,n,p)
@@ -16,7 +20,7 @@ leqbinomial <-function(y,n,p,imprimir){
     print(a)
   }else{a}
 }
-geqbinomial <-function(y,n,p,imprimir){
+geqbinomial <-function(y,n,p,imprimir){     #P(x>=y)
   a=0
   for(i in y:n){
     a<-a+dbinom(i,n,p)
@@ -25,7 +29,7 @@ geqbinomial <-function(y,n,p,imprimir){
     print(a)
   }else{a}
 }
-lgqbinomial <-function(y1,y2,n,p,imprimir){
+lgqbinomial <-function(y1,y2,n,p,imprimir){ #P(y1<=x<=y2)
   a=0
   for(i in y1:y2){
     a<-a+dbinom(i,n,p)
